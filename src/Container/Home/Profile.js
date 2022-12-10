93f92a0db041c9e6c7156112afea84d65bb6f9a9
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-10 11:51:54 +0600

    profiile section is under construction

---
+++ src/Container/Home/Profile.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <div className='profile-container'>
+      <div className='profile-parent'>
+        <div className='profile-details'>
+          <div className='colz'>
+            <div className='colz-icon'>
+              <a href="#">
+                <i className='fa fa-facebook-square'></i>
+              </a>
+              <a href="#">
+                <i className='fa fa-instagram'></i>
+              </a>
+              <a href="#">
+                <i className='fa fa-github'></i>
+              </a>
+            </div>
+          </div>
+          <div className='profile-details-name'>
+            <span className='primary-text'>
+              {" "}
+              Hello, I'M <span className='highlighted-text'>Hasan</span>
+            </span>
+          </div>
+          <div className='profile-details-role'>
+            <span className='primary-text'>
+              {" "}
+              <h1>
+                MERN Stack Developer
+              </h1>
+              <span>
+                do many wonderful website by modern technology
+              </span>
+            </span>
+          </div>
+          <div className='profile-options'>
+
+            <button className='btn primary-btn'>
+              {""}
+              Hire me{""}
+            </button>
+            <a href="resume.pdf" download='Md_Hasanul_Karim.pdf'>
+              <button className='btn highlighted-btn'>Get Resume</button>
+            </a>
+          </div>
+
+        </div>
+        <div className='profile-picture'>
+          <div className='profile-picture-background'>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Profile;
